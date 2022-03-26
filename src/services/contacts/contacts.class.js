711465078a3795a0ee52e358c5ca5de75716a169
mohamed  ismaiel <generalbuismaiel@gmail.com>
2022-03-27 01:42:02 +0200

    cutomize create method

--- src/services/contacts/contacts.class.js
+++ src/services/contacts/contacts.class.js
@@ -1,5 +1,11 @@
 const { Service } = require('feathers-mongoose');
 
 exports.Contacts = class Contacts extends Service {
-  
+    create(data, params){
+        const { name, mobileNumber } = data;
+        const user = params.user;
+        return super.create({mobileNumber, name, creator: user._id}, params)
+
+    }
+
 };
