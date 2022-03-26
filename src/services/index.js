53d50b28ca2d333fed8b327409cfaf32304cad5e
mohamed  ismaiel <generalbuismaiel@gmail.com>
2022-03-27 01:06:28 +0200

    create contact service

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const users = require('./users/users.service.js');
 const admins = require('./admins/admins.service.js')
+const contacts = require('./contacts/contacts.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(admins);
+  app.configure(contacts);
 };
